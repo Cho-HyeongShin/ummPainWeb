6016ff4ad5c4258cd5090ed89eadd83635a94942
Cho-HyeongShin <hgsin0102@gmail.com>
2019-12-08 12:22:12 +0900

    first commit
    gogogo

---
+++ pof/src/test/java/com/example/pof/PofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
